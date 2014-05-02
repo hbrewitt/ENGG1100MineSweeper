e3346a4ed472e6c2550d025c8510d1f7c0506ba4
Tom Manderson <TRManderson@gmail.com>
2014-05-02 18:28:23 +1000

    Setup complete in operational code

--- ENGG1100MineSweeper.ino
+++ ENGG1100MineSweeper.ino
@@ -1,7 +1,15 @@
 #include "HardwareLibrary.cpp"
 
-void setup(){
+Drive driveSystem(0x25, 7, 6, 4, 5);
+Magnetometer magSensor(0x1E, 500); //Not sure what the actual threshold will be - testing required
+IRSensor irSensor(A0);
+MarkerSystem marker(9, 13);
 
+void setup(){
+	driveSystem.init();
+	magSensor.init();
+	irSensor.init();
+	marker.init();
 }
 
 void loop(){
