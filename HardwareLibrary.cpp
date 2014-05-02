d1a1c52f8de31286c462b23d5ccef48ccd04e61d
Tom Manderson <TRManderson@gmail.com>
2014-05-02 17:27:01 +1000

    Generalised Gyro complete - not useable without full spec

--- HardwareLibrary.cpp
+++ HardwareLibrary.cpp
@@ -1,5 +1,27 @@
+#include <Wire.h>
+
 class Gyroscope{
-	//
+	int address;
+	public:
+		Gyroscope(int i2cAddress){ address=i2cAddress; }
+		void init(){
+			Wire.beginTransmission(address);
+			//Wire.write(0x00);
+			//Configuration registers for gyro go here
+			Wire.endTransmission();
+		}
+		int read(){
+			int returnVal;
+			Wire.beginTransmission(address);
+
+			Wire.endTransmission();
+
+			//Assuming from previous experience that value is 2 byte
+			Wire.requestFrom(address, 2);
+			while(Wire.available() < 2);
+			returnVal=Wire.read() <<8;
+			returnVal |= Wire.read();
+		}
 };
 
 class Magnetometer{
