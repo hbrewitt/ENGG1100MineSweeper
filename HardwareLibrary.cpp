e3c352fdfe7135459008f4c12d291dd79f113990
Tom Manderson <TRManderson@gmail.com>
2014-05-02 17:39:10 +1000

    Magnetometer class completed

--- HardwareLibrary.cpp
+++ HardwareLibrary.cpp
@@ -1,6 +1,7 @@
 #include <Wire.h>
 
 class Gyroscope{
+	//Tris10 TrackMotion
 	int address;
 	public:
 		Gyroscope(int i2cAddress){ address=i2cAddress; }
@@ -25,7 +26,43 @@ class Gyroscope{
 };
 
 class Magnetometer{
-	//
+	int address;
+	int threshold;
+	public:
+		Magnetometer(int i2cAddress, int mineThreshold){
+			threshold=mineThreshold;
+			address=i2cAddress;
+		}
+
+		void init(){
+			//Writes to registers for setup;
+
+			Wire.beginTransmission(address);
+			//Continuous mode
+			Wire.send(0x02);
+			Wire.send(0x00);
+			Wire.endTransmission();
+
+			Wire.beginTransmission(address);
+			//ODR=30Hz, 4 samples averaged per outpu
+			Wire.send(0x00);
+			Wire.send(0x34);
+			Wire.endTransmission();
+		}
+
+		int read(){
+			int val=0;
+			Wire.beginTransmission(address);
+			Wire.send(0x03); //x=3, y=5, z=7
+			Wire.endTransmission();
+			Wire.requestFrom(address, 2);
+			while (Wire.available() < 2);
+			val = Wire.receive()<<8; //msb
+			val |= Wire.receive(); //lsb
+			return val;
+		}
+
+		int binRead(){return (read > threshold ? 1 : 0);}
 };
 
 class IRSensor{
